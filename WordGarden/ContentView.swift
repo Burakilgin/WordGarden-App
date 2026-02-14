0233b838bb7ce99648d8f5a9f0e6ca651ec57af0
Burak Ilgin <burakilginnn@gmail.com>
2026-02-14 23:41:51 +0300

    Initial Commit

---
+++ WordGarden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordGarden
+//
+//  Created by Burak Ilgın on 14.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Burak Ilgın on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
